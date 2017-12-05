cf3e593c0191c660ef72da2c4b08cfa4dab5c7da
Silvio Angelo <silvioangels@gmail.com>
2017-12-05 15:35:58 -0200

    Aula 9 - Ajax - Capturando erros de requisição

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -7,12 +7,24 @@ var botaoBuscarPacientes = document.querySelector("#buscar-pacientes");
     xhr.open("GET","http://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load",function(){
 
-      var resposta = xhr.responseText;
-      var pacientes = JSON.parse(resposta);
+    var erroAjax = document.querySelector("#erro-ajax");
+    erroAjax.classList.add("invisivel");
 
-      pacientes.forEach(function(paciente){
+      if(xhr.status == 200){
+
+        var resposta = xhr.responseText;
+        var pacientes = JSON.parse(resposta);
+
+        pacientes.forEach(function(paciente){
           adicionaPacienteNaTabela(paciente);
-      });
+        });
+
+      }else {
+
+        erroAjax.classList.remove("invisivel");
+
+      }
+
 
     });
 
