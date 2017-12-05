d6b1aeda8fe5017bd387fe4918f92148d9e5fc8d
Silvio Angelo <silvioangels@gmail.com>
2017-12-05 08:19:08 -0200

    Aula 5 - Boas Praticas - Quebrando o codigo em arquivos

---
+++ js/calcula-imc.js
@@ -0,0 +1,40 @@
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+  var paciente = pacientes[i];
+
+  var tdPeso   = paciente.querySelector(".info-peso");
+  var peso     = tdPeso.textContent;
+
+  var tdAltura   = paciente.querySelector(".info-altura");
+  var altura     = tdAltura.textContent;
+
+  var tdImc   = paciente.querySelector(".info-imc");
+
+  var pesoEhValido = true;
+  var alturaEhValido = true;
+
+  if(peso < 0 || peso > 1000){
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido!!";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(altura < 0 || altura > 3.00){
+    alturaEhValido = false;
+    tdImc.textContent = "Altura Inválida!!";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if(pesoEhValido && alturaEhValido){
+    var imc = peso / (altura * altura);
+
+    tdImc.textContent = imc.toFixed(2);
+  }
+
+}
