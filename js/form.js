88f7f15dd75a6ced7b546327ba03b9e5b014c2c4
Silvio Angelo <silvioangels@gmail.com>
2017-12-05 08:45:45 -0200

    Aula 5 - Boas Praticas com Javascript - Criando um objeto paciente

--- js/form.js
+++ js/form.js
@@ -4,10 +4,8 @@ botaoAdicionar.addEventListener("click",function(event){
 
   var form = document.querySelector("#form-adiciona");
 
-  var nome    = form.nome.value;
-  var peso    = form.peso.value;
-  var altura  = form.altura.value;
-  var gordura = form.gordura.value;
+  var paciente = obtemPacienteDoFormulario(form);
+
 
   var pacienteTr = document.createElement("tr");
 
@@ -33,3 +31,17 @@ botaoAdicionar.addEventListener("click",function(event){
   tabela.appendChild(pacienteTr);
 
 });
+
+function obtemPacienteDoFormulario(form){
+
+  var paciente = {
+    nome:    form.nome.value,
+    peso:    form.peso.value,
+    altura:  form.altura.value,
+    gordura: form.gordura.value,
+    imc:     calculaImc(form.peso.value, form.altura.value)
+  }
+
+  return paciente;
+
+}
