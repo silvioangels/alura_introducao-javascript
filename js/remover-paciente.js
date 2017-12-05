41f5c0b54988ef5f9825382b5a81499cb841b86a
Silvio Angelo <silvioangels@gmail.com>
2017-12-05 10:41:43 -0200

    Aula 7 - Remoção, Delegação e Animação - Delegando Eventos

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,6 +1,16 @@
 var pacientes = document.querySelectorAll(".paciente");
-pacientes.forEach(function(paciente){
-  paciente.addEventListener("dblclick",function(){
-    this.remove();
-  })
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event){
+  var alvoEvento = event.target; // TR
+  var paiDoAlvo = alvoEvento.parentNode; //TL
+
+  paiDoAlvo.remove();
 });
+
+// pacientes.forEach(function(paciente){
+//   paciente.addEventListener("dblclick",function(){
+//     this.remove();
+//   })
+// });
